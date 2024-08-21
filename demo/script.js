958f15bc701279f5e15510f6f86c7d2edec47ef5
m-sarabi <m.sarabi.jkd@gmail.com>
2024-08-21 11:09:34 +0330

    reorganized the demo script.js

--- demo/script.js
+++ demo/script.js
@@ -1,47 +1,60 @@
+// Initialize the RainChar instance with default options
 const rain = new RainChar({
     id: 'rain',
     font: 'Verdana',
     parentId: 'effect',
-    // charRange: [0x3041, 0x30FF],
 });
 rain.start();
 
+// Utility function to restart the rain effect
+function restartRain() {
+    rain.start();
+}
+
+// Event listener for font change
 document.getElementById('font').addEventListener('change', function () {
     rain._font = this.value;
-    rain.start();
+    restartRain();
 });
 
-const charSize1 = document.getElementById('char-size1');
-const charSize2 = document.getElementById('char-size2');
-[charSize1, charSize2].forEach(charSize => {
-    charSize.addEventListener('change', function () {
-        rain._charSize = [charSize1.value ? Number(charSize1.value) : 10, charSize2.value ? Number(charSize2.value) : 40];
-        rain.start();
+// Event listener for character size change
+const charSizeInputs = [document.getElementById('char-size1'), document.getElementById('char-size2')];
+charSizeInputs.forEach(input => {
+    input.addEventListener('change', function () {
+        rain._charSize = charSizeInputs.map(input => input.value ? Number(input.value) : input.id === 'char-size1' ? 10 : 40);
+        restartRain();
     });
 });
 
+// Event listener for character range change
 document.getElementById('char-range').addEventListener('change', function () {
-    const range = this.value.split(' ').map(Number);
-    rain._charRange = [range[0], range[1]];
-    rain.start();
+    rain._charRange = this.value.split(' ').map(Number);
+    restartRain();
 });
 
-document.getElementById('bg-color').addEventListener('input', function () {
-    rain._bg = this.value;
+// Event listeners for color change (background and foreground)
+['bg-color', 'fg-color'].forEach(id => {
+    document.getElementById(id).addEventListener('input', function () {
+        rain[`_${id.replace('-color', '')}`] = this.value;
+        restartRain();
+    });
 });
 
-document.getElementById('fg-color').addEventListener('input', function () {
-    rain._fg = this.value;
-});
+// Utility function to clamp a value within a specified range
+function clamp(value, min, max) {
+    return Math.min(Math.max(min, value), max);
+}
 
+// Event listener for FPS change
 document.getElementById('fps').addEventListener('input', function () {
-    this.value = Math.min(Math.max(Number(this.getAttribute('min')), this.value), Number(this.getAttribute('max')));
+    this.value = clamp(Number(this.value), Number(this.getAttribute('min')), Number(this.getAttribute('max')));
     rain._fps = this.value;
-    rain.start();
+    restartRain();
 });
 
+// Event listener for density factor change
 document.getElementById('density').addEventListener('input', function () {
-    this.value = Math.min(Math.max(Number(this.getAttribute('min')), this.value), Number(this.getAttribute('max')));
+    this.value = clamp(Number(this.value), Number(this.getAttribute('min')), Number(this.getAttribute('max')));
     rain._densityFactor = this.value;
-    rain.start();
+    restartRain();
 });
